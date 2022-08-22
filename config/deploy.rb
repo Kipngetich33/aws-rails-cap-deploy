9822613be729654ee8cc519b9b4958751fc7a03e
Kipngetich33 <khalifngeno@gmail.com>
2022-08-22 21:28:14 +0300

    add changes for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,11 @@ lock "~> 3.14.0"
 require 'capistrano-db-tasks'
 
 #FIXME add your app name here
-set :application, 'your-app'
+set :application, 'aws-rails'
 #FIXME replace 'git@github.com:YOUR-GIT-REPO-HERE' with your git clone url
-set :repo_url, 'git@github.com:YOUR-GIT-REPO-HERE'
+set :repo_url, 'https://github.com/Kipngetich33/aws-rails-cap-deploy.git'
 #FIXME add location on the server here
-set :deploy_to, '/home/deploy/your-app'
+set :deploy_to, '/home/ubuntu/aws-rails'
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 set :linked_files, %w{config/database.yml config/master.key}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 #FIXME replace 1.2.3.4 with your IP address
-server '1.2.3.4', user: 'deploy', roles: %w{web app db}
+server '3.144.198.203', user: 'ubuntu', roles: %w{web app db}
 set :rails_env, 'production'
